0da3e7572c6bc1f809cc9a6cda63100c388f6347
c15d4fd75e0ed509b74d19ab47202796 <111346354+c15d4fd75e0ed509b74d19ab47202796@users.noreply.github.com>
2025-06-08 15:19:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloFriend;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Friend!");
+        string decryptKey = "j3e8ql585qhg99iwld9j;70o9qew;gqw8o8wil0er";
+        Console.Write("l");
+        while (true)
+        {
+            Console.Write("ol");
+            Thread.Sleep(10);
+            decrypt(decryptKey);
+        }
+
+    }
+
+    static void decrypt(string key)
+    {
+        // Oops, been too BUSY to finish this - sorry!!! Guess you'll just have to work it out yourself!
+        // Time's running out, so you'd better get a shift on!!!
+        // DOWN WITH THE [REDACTEDREDACTEDRED], UP THE HELMSMAN!!!!! :D
+    }
+}
